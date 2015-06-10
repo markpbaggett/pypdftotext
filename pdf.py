8e53ee2365323b97ef8f93cb6795c88399048ff0
Mark Baggett <mbagget1@utk.edu>
2015-06-10 14:07:56 -0400

    Add optionparsing to specify filename and output filename

--- pdf.py
+++ pdf.py
@@ -1,9 +1,27 @@
 import PyPDF2
-pdfFileOBJ = open('testing.pdf', 'rb')
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="fileName", help="specify PDF to extract from")
+parser.add_option("-o", "--output", dest="outputName", help="specify output filename to write text to")
+
+(options, args) = parser.parse_args()
+if options.fileName is None or options.outputName is None:
+        parser.print_help()
+        parser.error("a PDF name and an extract name are required!!!")
+
+if options:
+	fileName = outputName = ''
+	if options.fileName:
+		fileName = options.fileName
+	if options.outputName:
+		outputName = options.outputName
+
+pdfFileOBJ = open(fileName, 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileOBJ)
 numPages = pdfReader.numPages
-print numPages
-f = open('test.txt', 'w')
+print 'Extracting %d pages.' % numPages
+f = open(outputName, 'w')
 i = 0
 while i < numPages:
 	pageObj = pdfReader.getPage(i)
